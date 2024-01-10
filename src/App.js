b12e487c2b4ef56a3456b257828882726827fe34
nguyenle2k3 <nguyenle2k31412@gmail.com>
2024-01-10 14:41:05 +0700

    9.3.3 - conplete form login * responsive layout

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
-import './App.scss';
-import Nav from './components/Navigation/Nav';
-import Login from './components/Login/Login';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import "./App.scss";
+import Nav from "./components/Navigation/Nav";
+import Login from "./components/Login/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <div className="app-container">
-        {/* <Nav /> */}
-        <Switch>
-          <Route path="/news">
-            News
-          </Route>
-          <Route path="/about">
-            About
-          </Route>
-          <Route path="/contact">
-            Contact
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/" exact>
-            Home
-          </Route>
-          <Route path="*">
-            404 not found
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="app-container">
+                {/* <Nav /> */}
+                <Switch>
+                    <Route path="/news">News</Route>
+                    <Route path="/about">About</Route>
+                    <Route path="/contact">Contact</Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/" exact>
+                        Home
+                    </Route>
+                    <Route path="*">404 not found</Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,45 @@
+import "./Login.scss";
 const Login = (props) => {
-    return (
-        <div className="login-container">
-            login
-        </div>
-    )
-}
+	return (
+		<div className="login-container">
+			<div className="container">
+				<div className="row px-3 px-sm-0">
+					<div className="content-left col-12 d-none col-sm-7 d-sm-block">
+						<div className="title">facebook</div>
+						<div className="details">
+							Facebook helps you connect and share with the people
+							in your life.
+						</div>
+					</div>
+					<div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+						<div className="title d-sm-none">facebook</div>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Email address or phone number"
+						/>
+						<input
+							type="password"
+							className="form-control"
+							placeholder="Password"
+						/>
+						<button className="btn btn-primary">Login</button>
+						<span className="text-center">
+							<a href="#" className="forgot-password">
+								Forgot your password?
+							</a>
+						</span>
+						<hr />
+						<div className="text-center">
+							<button className="btn btn-success">
+								Create new account
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Login;
